23bb6415981c5f342d839833d2f89943878e7716
Aashirwad Sinha <aashirwad@ratibpal.com>
2019-11-17 01:05:50 +0530

    Added collection view holding scrollable emojis

---
+++ EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Aashirwad Sinha on 11/17/19.
+//  Copyright © 2019 Credit Suisse. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
